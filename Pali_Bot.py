a06eda3eb6cdca5d18c6eb4abcb018bedc436428
Kotebus <104275926+Kotebus@users.noreply.github.com>
2022-04-24 21:38:07 +0300

    Update Pali_Bot.py
    Exceptions handling

--- Pali_Bot.py
+++ Pali_Bot.py
@@ -15,23 +15,32 @@ main_menu = """
 >>> /dhammapada_sutta - ИЗ ДХАММАПАДЫ
 >>> /theragatha_sutta - ИЗ ТХЕРАГАТХИ"""
 
+def get_text(sitemap, command):
+    try:
+        print_text = mainconfig(sitemap)
+    except Exception as e:
+        print_text = e
+
+    return print_text + '\n\n>>> СЛЕДУЮЩАЯ СУТТА:  /' + command + '\n>>> МЕНЮ: /start'
+
 @bot.message_handler(commands=['start'])
 def main_menuu(message):
-    bot.send_message(message.chat.id, main_menu)\
+    bot.send_message(message.chat.id, main_menu) \
 
 @bot.message_handler(commands=['all_sutta'])
 def all_suttaa(message):
-    print_text = mainconfig("sitemap.xml")
-    bot.send_message(message.chat.id, print_text + '\n\n>>> СЛЕДУЮЩАЯ СУТТА:  /all_sutta\n>>> МЕНЮ: /start')
+    print_text = get_text("sitemap.xml", 'all_sutta')
+    bot.send_message(message.chat.id, print_text)
 
 @bot.message_handler(commands=['theragatha_sutta'])
 def theragatha_suttaa(message):
-    print_text = mainconfig("sitemap_theragatha.xml")
-    bot.send_message(message.chat.id, print_text + '\n\n>>> СЛЕДУЮЩАЯ СУТТА:  /theragatha_sutta\n>>> МЕНЮ: /start')
+    print_text = get_text("sitemap_theragatha.xml", 'theragatha_sutta')
+    bot.send_message(message.chat.id, print_text)
 
 @bot.message_handler(commands=['dhammapada_sutta'])
 def dhammapada_suttaa(message):
-    print_text = mainconfig("sitemap_dhammapada.xml")
-    bot.send_message(message.chat.id, print_text + '\n\n>>> СЛЕДУЮЩАЯ СУТТА:  /dhammapada_sutta\n>>> МЕНЮ: /start')
+    print_text = get_text("sitemap_dhammapada.xml", 'dhammapada_sutta')
+    bot.send_message(message.chat.id, print_text)
 
-bot.polling(none_stop=True)	
+
+bot.polling(none_stop=True)
